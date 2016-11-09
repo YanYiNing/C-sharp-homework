2fb50c10111500c64889630f7290612a7d879050
YanYiNing <2837786061@qq.com>
2016-11-09 22:49:04 +0800

    添加项目文件。

---
+++ homework5/ComparerByStudentID.cs
@@ -0,0 +1,22 @@
+﻿using c__sharp__test;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homework5
+{
+    class ComparerByStudentID : IComparer<Student>
+    {
+        public int Compare(Student x, Student y)
+        {
+            if (x.studentID > y.studentID)
+                return 1;
+            else if (x.studentID < y.studentID)
+                return -1;
+            else
+                return 0;
+        }
+    }
+}
---
+++ homework5/MyArray.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace c__sharp__test
+{
+    class MyArray : System.Collections.CollectionBase
+    {
+
+        public int Add(object sender)
+        {
+            return List.Add(sender);
+        }
+        public void Remove(object sender)
+        {
+            List.Remove(sender);
+        }
+        public object this[int Index]
+        {
+            set
+            {
+                List[Index] = value;
+            }
+            get
+            {
+                return (object)List[Index];
+            }
+
+        }
+        public new void Clear()
+        {
+            List.Clear();
+        }
+    }
+}
