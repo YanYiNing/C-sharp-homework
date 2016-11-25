931df725df0b4c19e4489e9b1c8f00e54d12a1ac
YanYiNing <2837786061@qq.com>
2016-11-25 23:27:43 +0800

    homework6

---
+++ Homework6/Homework6/WaterHeater.cs
@@ -0,0 +1,34 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Homework6
+{
+    
+    class WaterHeater
+    {
+        private int waterTemperature = 20;
+      
+        public bool Heating()
+        {
+            if (waterTemperature < 80)
+            {
+                Console.WriteLine("The temperature of water is " + ++waterTemperature);
+                return true;
+            }
+            else
+            {
+                Console.WriteLine("Stop!Because the temperature of water is 80 degrees centigrade!");
+                return false;
+            }
+        }
+        public static void Main(String[] args)
+        {
+            WaterHeater waterHeater = new WaterHeater();
+            while(waterHeater.Heating());
+        }
+    }
+}
