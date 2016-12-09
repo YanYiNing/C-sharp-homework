5dae5a616922dfd8acbcd6bfa1eed551f2dcb6e3
YanYiNing <2837786061@qq.com>
2016-12-09 22:14:15 +0800

    homework8

---
+++ Search/Search/MainPage.xaml.cs
@@ -0,0 +1,64 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// “空白页”项模板在 http://go.microsoft.com/fwlink/?LinkId=234238 上有介绍
+
+namespace Search
+{
+    /// <summary>
+    /// 可用于自身或导航至 Frame 内部的空白页。
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        ObservableCollection<Book> list = new ObservableCollection<Book>();
+       
+        private async void SearchBox_QuerySubmitted(AutoSuggestBox sender, AutoSuggestBoxQuerySubmittedEventArgs args)
+        {
+            Uri uri = new Uri("https://api.douban.com/v2/book/search?q=" + Search.Text);
+            string content = "";
+            await Task.Run(() =>
+            {
+                HttpClient httpClient = new HttpClient();
+                System.Net.Http.HttpResponseMessage response;
+                response = httpClient.GetAsync(uri).Result;
+                if (response.StatusCode == HttpStatusCode.OK)
+                    content = response.Content.ReadAsStringAsync().Result;
+            });
+            JObject jsonobj = JObject.Parse(content);//解序列化转化成json对象
+            string json = jsonobj["books"].ToString();
+            list = JsonConvert.DeserializeObject<ObservableCollection<Book>>(json);
+            BookList.ItemsSource = list;
+        }//添加搜索按钮的事件
+
+    }
+
+    class Book
+    {
+        public string[] author { get; set; }
+        public string title { get; set; }
+        public string pubdate { get; set; }
+    }
+}
